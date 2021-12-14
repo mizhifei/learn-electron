fc42fa87ab7a609b8892eabe6cade218792150cf
mizf <mizf@cndatacom.com>
2021-12-14 15:01:12 +0800

    ch2 dark mode

--- ch2.darkMode/preload.js
+++ ch2.darkMode/preload.js
@@ -4,3 +4,6 @@ contextBridge.exposeInMainWorld("darkMode", {
   toggle: () => ipcRenderer.invoke("dark-mode:toggle"),
   system: () => ipcRenderer.invoke("dark-mode:system"),
 });
+
+
+console.log("hi preload.js");
